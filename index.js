a811ee0542e1e2225bbe40795350b1a075e4fd34
MsDiala <diala-is-diala@hotmail.com>
2023-10-15 21:51:11 +0100

    done

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const name = "Joe";
-const height = 74;
+// const name = "Joe";
+// const height = 74;
+// const message = `${name} is ${height} inches tall`;
+
+// module.exports = {name, height, message};
+
+const name = "Susan";  // Change "Joe" to "Susan"
+const height = 39;      // Change 74 to 39
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
+
